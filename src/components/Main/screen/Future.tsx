1de0057ea60fec9f31687bdf4e5652774b7afcd5
Mitty0719 <sprout071900@gmail.com>
2023-05-27 21:45:09 +0900

    Append Future Text Effects

--- src/components/Main/screen/Future.tsx
+++ src/components/Main/screen/Future.tsx
@@ -1,15 +1,27 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
 
 const FutureCon = styled.section`
   position: relative;
   height: 100vh;
 `;
-const FutureTextCon = styled.div`
+const FutureVerticalBackground = styled.div`
   position: absolute;
-  bottom: 80px;
-  right: 80px;
-  font-size: 96px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(90deg, #fafafa 5%, rgba(0, 0, 0, 0) 0);
+  background-size: calc(100% / 8) 100%;
+`;
+const FutureHorizontalBackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, #fafafa 5%, rgba(0, 0, 0, 0) 0);
+  background-size: 150px calc(100% / 6);
 `;
 const AsteriskCon = styled.div`
   @keyframes asterisk-con-rotate {
@@ -22,7 +34,7 @@ const AsteriskCon = styled.div`
   left: 180px;
   animation: asterisk-con-rotate 12s infinite linear;
   transform-origin: calc(64px / 2) calc(64px / 2);
-`
+`;
 const AsteriskIcon = styled.img`
   @keyframes asterisk-rotate {
     0% { transform: rotate(0deg) }
@@ -50,19 +62,93 @@ const AsteriskIcon = styled.img`
     left: 64px;
     animation-duration: 10s;
   }
+`;
+
+const FutureTextCon = styled.div`
+  position: absolute;
+  bottom: 80px;
+  right: 80px;
+  font-size: 96px;
+  font-weight: bold;
+  letter-spacing: 10px;
+`;
+const IconText = styled.div`
+  line-height: 116px;
 `
+const FutureIcon = styled.span`
+  display: inline-block;
+  min-width: 320px;
+`;
+const ChangedText = styled.div`
+  min-height: 116px;
+  line-height: 116px;
+  text-align: left;
+  cursor: pointer;
+`;
 
 const Future : FunctionComponent = function () {
+  
+  const futureList = ['Developer', 'Leader', 'Photographer', 'Mentor'];
+  const futureColor = ['#fac901', '#225095', '#dd0100', '#fac901'];
+  const dummyLetterList = ['#', '$', '%', '@', '~', '&', '+', '-', '=', '/'];
+  const [futureTextIndex, setFutureTextIndex] = useState(0);
+  const [changedText, setChangedText] = useState('');
+  const [changedTextColor, setChangedTextColor] = useState({});
+  let changedLetterIndex = 0;
+  const [textChangeInterval, setTextChangeInterval] = useState(setInterval(()=>{}, 9999999));
+
+  useEffect(() => {
+    setFutureTextIndex(0);
+
+  }, []);
+
+  useEffect(() => {
+    clearInterval(textChangeInterval);
+    const targetText = futureList[futureTextIndex];
+    setChangedTextColor({color: futureColor[futureTextIndex].toString()});
+    typeTextEffect(targetText);
+
+    setTimeout(() => {
+      if(futureTextIndex >= futureList.length - 1) {
+        setFutureTextIndex(0);
+      } else {
+        setFutureTextIndex(futureTextIndex + 1);
+      }
+    }, 5000)
+  }, [futureTextIndex]);
+
+
+  function typeTextEffect(targetText : String) {
+
+    const interval = setInterval(() => {
+      let showText = '';
+      for(let i = 0; i < targetText.length; i++) {
+        if(i <= changedLetterIndex) {
+          showText += targetText[i];
+        } else {
+          showText += dummyLetterList[Math.round(Math.random() * (dummyLetterList.length - 1))];
+        }
+      }
+
+      setChangedText(showText.toString());
+      changedLetterIndex++;
+    }, 80);
+
+    setTextChangeInterval(interval);
+  };
+
   return (
     <FutureCon>
+      <FutureVerticalBackground />
+      <FutureHorizontalBackground />
       <AsteriskCon>
         <AsteriskIcon src="../../../../image/icon_asterisk01.svg" alt="" />
         <AsteriskIcon src="../../../../image/icon_asterisk02.svg" alt="" />
         <AsteriskIcon src="../../../../image/icon_asterisk03.svg" alt="" />
       </AsteriskCon>
       <FutureTextCon>
-        <div>I'll be (<span></span>)</div>
-        <div>Developer</div>
+        <IconText>I'll be (<FutureIcon></FutureIcon>)</IconText>
+        <ChangedText style={changedTextColor}>{changedText}</ChangedText>
       </FutureTextCon>
     </FutureCon>
   );
--- src/components/Main/screen/Pictures.tsx
+++ src/components/Main/screen/Pictures.tsx
@@ -1,9 +1,36 @@
 import { FunctionComponent } from "react";
 import styled from "@emotion/styled";
 
+const PictureCon = styled.section`
+
+`
+const PictureContent = styled.article`
+  display: flex;
+  justify-content: center;
+`;
+const PictureBox = styled.div`
+  width: 640px;
+  height: 2400px;
+  background-color: #FFE5DF;
+  box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.25);
+  transform: translateY(-40px);
+`;
+
+const PictureTextCon = styled.div`
+`;
+
+
 const Pictures : FunctionComponent = function () {
   return (
-    <div></div>
+    <PictureCon>
+      <PictureContent>
+        <PictureBox>
+
+        </PictureBox>
+        <PictureTextCon>
+        </PictureTextCon>
+      </PictureContent>
+    </PictureCon>
   );
 
 };
