3818faddf161b7d31fb543b3c22082d7cceb1430
Mitty0719 <sprout071900@gmail.com>
2023-05-28 09:43:17 +0900

    Append Pictures

--- src/components/Main/screen/Pictures.tsx
+++ src/components/Main/screen/Pictures.tsx
@@ -3,20 +3,44 @@ import styled from "@emotion/styled";
 
 const PictureCon = styled.section`
 
-`
-const PictureContent = styled.article`
-  display: flex;
-  justify-content: center;
-`;
-const PictureBox = styled.div`
-  width: 640px;
-  height: 2400px;
-  background-color: #FFE5DF;
-  box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.25);
-  transform: translateY(-40px);
 `;
 
-const PictureTextCon = styled.div`
+const PictureContent = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-end;
+`;
+const PictureTitleCon = styled.div`
+  width: 400px;
+  border-bottom: solid 4px #000;
+`;
+const PictureTitle = styled.h3`
+  font-size: 36px;
+  text-align: right;
+`;
+const PictureBox = styled.div`
+  margin-top: 30px;
+`;
+const PictureList = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 2px;
+`;
+const PictureItem = styled.li`
+  position: relative;
+  display: inline-block;
+  flex: 1;
+  min-width: 380px;
+  height: 360px;
+  background-color: #ccc;
+  overflow: hidden;
+`;
+const PictureImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  height: 100%;
+  transform: translate(-50%, -50%);
 `;
 
 
@@ -24,11 +48,31 @@ const Pictures : FunctionComponent = function () {
   return (
     <PictureCon>
       <PictureContent>
+        <PictureTitleCon>
+          <PictureTitle>PHOTOS</PictureTitle>
+        </PictureTitleCon>
         <PictureBox>
-
+          <PictureList>
+            <PictureItem>
+              <PictureImage src="../../../../picture/picture_station.jpg"></PictureImage>
+            </PictureItem>
+            <PictureItem>
+              <PictureImage src="../../../../picture/picture_sky.jpg"></PictureImage>
+            </PictureItem>
+            <PictureItem>
+              <PictureImage src="../../../../picture/picture_persimmon.jpg"></PictureImage>
+            </PictureItem>
+            <PictureItem>
+              <PictureImage src="../../../../picture/picture_traditional_market.jpg"></PictureImage>
+            </PictureItem>
+            <PictureItem>
+              <PictureImage src="../../../../picture/picture_morning_ocean.jpg"></PictureImage>
+            </PictureItem>
+            <PictureItem>
+              <PictureImage src="../../../../picture/picture_tunnel.jpg"></PictureImage>
+            </PictureItem>
+          </PictureList>
         </PictureBox>
-        <PictureTextCon>
-        </PictureTextCon>
       </PictureContent>
     </PictureCon>
   );
--- src/components/Main/screen/Scroll.tsx
+++ src/components/Main/screen/Scroll.tsx
@@ -1,9 +1,35 @@
 import { FunctionComponent } from "react";
 import styled from "@emotion/styled";
 
+const ScrollCon = styled.section`
+
+`
+const ScrollContent = styled.article`
+  display: flex;
+  justify-content: center;
+`;
+const ScrollBox = styled.div`
+  width: 640px;
+  height: 2400px;
+  background-color: #FFE5DF;
+  box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.25);
+  transform: translateY(-40px);
+`;
+
+const ScrollTextCon = styled.div`
+`;
+
 const Scroll : FunctionComponent = function () {
   return (
-    <div></div>
+    <ScrollCon>
+      <ScrollContent>
+        <ScrollBox>
+
+        </ScrollBox>
+        <ScrollTextCon>
+        </ScrollTextCon>
+      </ScrollContent>
+    </ScrollCon>
   );
 
 };
