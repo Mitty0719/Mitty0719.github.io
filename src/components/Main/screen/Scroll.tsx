1ee9c85ea733ec61c3813b96366d987e63c09ce4
Mitty0719 <sprout071900@gmail.com>
2023-06-08 21:58:45 +0900

    Append Video Track for Canvas

--- src/components/Main/screen/Scroll.tsx
+++ src/components/Main/screen/Scroll.tsx
@@ -6,91 +6,152 @@ const ScrollCon = styled.section`
 `
 const ScrollContent = styled.article`
   display: flex;
-  justify-content: center;
+  flex-flow: column;
+  align-items: center;
 `;
-const ScrollBox = styled.div`
+const FadeBoxCon = styled.div`
   width: 50vw;
-  height: 2400px;
-  background-color: #fdfdfd;
-  box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.25);
   transform: translateY(-40px);
 `;
-
-const ScrollTextCon = styled.div`
+const FadeCanvas = styled.canvas`
   position: absolute;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
-  top: 0;
-  left: 0;
-  padding: 0 80px;
   width: 100%;
-  height: 90%;
+  height: 900px;
 `;
-const ScrollTextList = styled.ul`
-  position: absolute;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  top: 0%;
-  left: 50%;
-  height: 100%;
-  writing-mode: vertical-rl;
-  text-orientation: mixed;
-  &:first-of-type {
-    transform: translateX(calc(-100% - 25vw - 36px));
-    li {
-      transform: rotate(180deg);
+const FadeItem = styled.div`
+  position: relative;
+  width: 100%;
+  margin-bottom: 20px;
+  background-color: #fdfdfd;
+  box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.25);
+  transition-duration: 1s;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0);
+  }
+  &::after {
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 20px;
+    transform: translateY(100%);
+    background-color: #fff;
+    content: '';
+  }
+  &:last-child {
+    margin-bottom: 0;
+    &:after {
+      content: none;
     }
   }
-  &:last-of-type {
-    transform: translateX(calc(25vw + 36px));
-  }
 `;
-const ScrollTextItem = styled.li`
-  font-size: 36px;
-  font-weight: 800;
+const ScrollBox = styled.div`
+  width: 100vw;
 `;
-
 const ScrollBoxItem = styled.img`
   position: absolute;
   width: 200px;
+  transition-duration: 1s;
 `;
 
 const Scroll : FunctionComponent = function () {
 
   const scrollItemList = [...Array(4)].map(() => useRef(null));
+  const fadeItemHeightList = [300, 200, 120, 80, 60, 40];
+  const fadeCanvas = useRef(null);
+  let canvas : HTMLCanvasElement | null = null;
+  let ctx : CanvasRenderingContext2D | null = null;
+  let canvasWidth = 0;
+  let canvasHeight = 0;
+  const imageCnt = 95;
+  const canvasImageList : Array<HTMLImageElement> = [];
+  let canvasImageSequence = 0;
+  let loopIndex = 0;
+
   useEffect(() => {
-    const observer = new IntersectionObserver((item) => {
-      // do something
+    const observer = new IntersectionObserver((entries, observe) => {
+      entries.forEach(entry => {
+        const target = entry.target as HTMLElement;
+        
+        if(entry.isIntersecting) {
+        } else {
+          target.style.transform = 'translateY(0)';
+        }
+
+      });
     });
+
     for(const scrollItem of scrollItemList) {
       if(scrollItem.current) {
         observer.observe(scrollItem.current);
       }
     }
+
+    if(fadeCanvas.current) {
+      canvas = fadeCanvas.current as HTMLCanvasElement;
+      ctx = canvas.getContext('2d');
+      initCanvas();
+    }
+
+    loadImages();
   }, []);
+
+  function initCanvas() {
+    if(canvas) {
+      canvasWidth = canvas.clientWidth;
+      canvasHeight = canvas.clientHeight;
+    }
+    if(ctx) {
+      window.requestAnimationFrame(canvasLoop);
+    }
+  }
+
+  function canvasLoop() {
+    window.requestAnimationFrame(canvasLoop);
+    ctx!.clearRect(0, 0, canvasWidth, canvasHeight);
+    // 중간에 위치 하게 계산하기
+    ctx!.drawImage(canvasImageList[canvasImageSequence], 0, 0, 300,  200);
+    if(!(loopIndex % 10)) {
+      canvasImageSequence++;
+    }
+    if(canvasImageSequence == imageCnt) {
+      canvasImageSequence = 0;
+    }
+    loopIndex++;
+  }
+
+  function loadImages() {
+    const prefix = `../../../../video/fadebox/IMG_2417_out`;
+    
+    let image = null;
+    for(let i = 1; i <= imageCnt; i++ ) {
+      image = new Image;
+      image.src = prefix + (String(i).padStart(4, '0')) + '.jpg';
+      canvasImageList.push(image);
+    }
+  }
 
   return (
     <ScrollCon>
       <ScrollContent>
-        <ScrollTextCon>
-          <ScrollTextList>
-            <ScrollTextItem>Bike</ScrollTextItem>
-            <ScrollTextItem>Constant</ScrollTextItem>
-            <ScrollTextItem>NERD</ScrollTextItem>
-          </ScrollTextList>
-          <ScrollTextList>
-            <ScrollTextItem>Steady</ScrollTextItem>
-            <ScrollTextItem>고구마</ScrollTextItem>
-            <ScrollTextItem>Fassional</ScrollTextItem>
-            <ScrollTextItem>Picture</ScrollTextItem>
-          </ScrollTextList>
-        </ScrollTextCon>
+        <FadeBoxCon>
+            <FadeCanvas ref={fadeCanvas}/>
+          {fadeItemHeightList.map(height => <FadeItem key={height} style={{height: height}}/>)}
+        </FadeBoxCon>
         <ScrollBox>
-          <ScrollBoxItem ref={scrollItemList[0]} src="../../../../image/bicycle.svg" style={{top: '500px'}}/>
-          <ScrollBoxItem ref={scrollItemList[1]} src="../../../../image/sweetpotato.svg" style={{top: '800px'}} />
-          <ScrollBoxItem ref={scrollItemList[2]} src="../../../../image/camera.svg" style={{top: '1600px'}}/>
+          <ScrollBoxItem 
+            ref={scrollItemList[0]} 
+            src="../../../../image/bicycle.svg" 
+          />
+          <ScrollBoxItem 
+            ref={scrollItemList[1]} 
+            src="../../../../image/sweetpotato.svg" 
+          />
+          <ScrollBoxItem 
+            ref={scrollItemList[2]} 
+            src="../../../../image/camera.svg" 
+          />
         </ScrollBox>
       </ScrollContent>
     </ScrollCon>
