fc71afb3d9c1ae457ba309d75216e788572f2e3c
Mitty0719 <sprout071900@gmail.com>
2023-06-23 20:04:55 +0900

    Updates

--- js/scrollEvent.js
+++ js/scrollEvent.js
@@ -2,7 +2,6 @@ class ScrollEvent {
   constructor() {
     this.sectionNodeList = document.querySelectorAll('.scroll-section');
     this.scrollSectionList = [];
-    this.scrollIndex = 0;
     
     this.init();
   }
@@ -21,14 +20,9 @@ class ScrollEvent {
   }
 
   scrollLoop() {
-    // this.scrollSectionList[this.scrollIndex].scrollLoop(scrollY, this.addScrollIndex.bind(this));
     for(const section of this.scrollSectionList) {
-      section.scrollLoop(scrollY, this.addScrollIndex.bind(this));
+      section.scrollLoop(scrollY);
     }
-  }
-
-  addScrollIndex(value) {
-    this.scrollIndex += value;
   }
 
   addScrollEvent(options) {
@@ -68,30 +62,27 @@ class ScrollSection {
     this.node = node;
     this.width = this.node.offsetWidth;
     this.height = this.node.offsetHeight;
-    this.startY = prevHeight;
+    this.startY = this.node.offsetTop;
     this.endY = this.startY + this.node.offsetHeight;
     this.ratio = 0;
     this.events = [];
   }
 
-  scrollLoop(scrollY, addScrollIndex) {
+  scrollLoop(scrollY) {
     // calcHeight
     const targetY = scrollY - this.startY;
     let ratio = targetY / this.height;
-
-    if(ratio < 0) {
-      ratio = 0;
-      addScrollIndex(-1);
-    } else if(ratio > 1) {
-      ratio = 1;
-      addScrollIndex(1);
-    }
     this.ratio = ratio;
-
+    console.log(scrollY, this.startY);
     // callEvents
     for(const event of this.events) {
       event();
     }
+  }
+  
+  checkScrollInSection(scrollY) {
+    if(scrollY > this.startY && scrollY < this.endY) return true;
+    return false;
   }
 
   addScrollEvent({id, style, startRatio, endRatio, startValue, endValue, suffix = ''}) {
@@ -144,7 +135,7 @@ class ScrollSection {
   addScrollClass({id, classList, startRatio, endRatio = 1}) {
     const element = this.node.querySelector(`#${id}`);
     const event = function() {
-      if(this.ratio >= startRatio && this.ratio <= endRatio) {
+      if(this.ratio > startRatio && this.ratio < endRatio) {
         for(const className of classList) {
           element.classList.add(className);
         }
